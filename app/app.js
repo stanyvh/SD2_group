a806a152ebbab89852d007664442e1179d2f61f4
Sarah Azam <azamsarah74@gmail.comgit config --global user.name Sarah>
2022-03-04 14:44:38 +0000

    hello

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("A test to see everything works!");
+    res.send("Stacy is such a beautiful name!");
 });
 
 // Create a route for testing the db

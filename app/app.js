2081dc74e143c0c188de3a6e2a72b4bd4960513d
Sarah Azam <azamsarah74@gmail.comgit config --global user.name Sarah>
2022-03-14 16:16:25 +0000

    backend updates sarah

--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
 // Import express.js
 const express = require("express");
 
-const { Teacher } = require('./teacher.js')
-
 // Create express app
 var app = express();
 
@@ -24,6 +22,8 @@ app.use(express.static("static"));
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+
+const { Teacher } = require('./teacher.js')
 
 // Create a route for root - /
 app.get("/", function(req, res) {
@@ -50,32 +50,13 @@ app.get("/skills", function(req, res) {
     });
 });
 
-
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
-});
-
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
+// Create a single teacher page
+app.get("/single-teacher/:id", async function (req, res) {
+    var T_ID = req.params.id;
+    var teacher = new Teacher(T_ID);
+    await teacher.getTeacherName();
+    console.log(teacher);
+    res.render('teacher', {teacher:teacher});
 });
 
 // Start server on port 3000
--- app/teacher.js
+++ app/teacher.js
@@ -1,3 +1,5 @@
+const db = require('./services/db');
+
 class Teacher {
     // Attributes
     teachID;
@@ -5,33 +7,21 @@ class Teacher {
     lastName;
     skills;
 
-    constructor(T_ID, firstName, lastName) {
+    constructor(T_ID, firstName, lastName, skills) {
         this.teachID = teachID;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.skills = [];
+        this.skills = skills;
     }
 
-    getFormattedName() {
-        return this.firstName + ' ' + this.lastName;
+    //Get the teacher name from the database
+    async getTeacherName() {
+        if (typeof this.firstName !== 'string') {
+            var sql = "SELECT * Teacher where id = ?"
+            const results = await db.query(sql, [this.id]);
+            this.firstName = results[0].name;
+        }
     }
-
-    getfirstName() {
-        return this.#firstName;
-    }
-
-    getlastName() {
-        return this.#lastName
-    }
-
-    setfirstName(firstName) {
-        this.#firstName = firstName;
-    }
-
-    setlastName(lastName) {
-        this.#lastName = lastName;
-    }
-
 }
 
 module.exports = {

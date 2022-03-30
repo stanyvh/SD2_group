f47ff8210f5c2bbcf580615cfe4dee94ac0f7b42
vicxstar <vickiayoade@gmail.com>
2022-03-30 09:42:58 +0000

    Register, login and logout pages complete. Just need to decorate x

--- app/app.js
+++ app/app.js
@@ -13,17 +13,39 @@ app.set('views', './app/views');
 
 // We require the db.js file to set up the database connection.
 const db = require('./services/db');
+app.use(express.urlencoded({ extended: true }));
 
 //Get the models
 const { Teacher } = require("./models/teacher");
+const { User } = require("./models/user");
 
 // Get the programmes dropdown menu
 const getskills = require("./models/getskills");
 
 //***********************************************************************************
 
+// Set the sessions
+var session = require('express-session');
+app.use(session({
+  secret: 'secretkeysdfjsflyoifasd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 // Create a route for root - /
 app.get("/", function(req, res) {
+    console.log(req.session);
+    if (req.session.uid) {
+		res.send('Welcome back, ' + req.session.uid + '!');
+	} else {
+		res.send('Please login to view this page!');
+	}
+	res.end();
+});
+
+// Create a route for 'index'
+app.get("/index", function(req, res) {
     res.render("index",
         {'title':'Profile Page', 'heading': 'heading'});
 });
@@ -37,6 +59,71 @@ app.get("/all-teachers", function(req, res) {
         res.render('all-teachers', {data:results});
     });
 });
+
+// Route for 'register.pug'
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+// Set password route
+app.post('/set-password', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            // If a valid, existing user is found, set the password and redirect to the users single-student page
+            await user.setUserPassword(params.password);
+            res.redirect('/single-teacher/' + uId);
+        }
+        else {
+            // If no existing user is found, add a new one
+            newId = await user.addUser(params.email);
+            res.send('Perhaps a page where a new user sets a programme would be good here');
+        }
+    } catch (err) {
+        console.error(`Error while adding password `, err.message);
+    }
+});
+
+// Route for 'login.pug'
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            match = await user.authenticate(params.password);
+            if (match) {
+                // Set the session for the user
+                req.session.uid = uId;
+                req.session.loggedIn = true;
+                console.log(req.session);
+                res.redirect('/single-teacher/' + uId);
+            }
+            else {
+                // TODO improve the user journey here
+                res.send('invalid password');
+            }
+        }
+        else {
+            res.send('invalid email');
+        }
+    } catch (err) {
+        console.error(`Error while comparing `, err.message);
+    }
+});
+
+// Route for Logout
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/login');
+  });
 
 
 app.get("/single-teacher/:id", async function(req, res) {
